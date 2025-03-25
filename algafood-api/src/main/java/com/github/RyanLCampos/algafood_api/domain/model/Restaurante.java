eaed3276d6c747665804427e0e8fd29f890f43ed
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-24 21:31:15 -0300

    :sparkles: Feat: cria entidades Cozinha e Restaurante

--- algafood-api/src/main/java/com/github/RyanLCampos/algafood_api/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/github/RyanLCampos/algafood_api/domain/model/Restaurante.java
@@ -1,4 +1,30 @@
 package com.github.RyanLCampos.algafood_api.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
 public class Restaurante {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
