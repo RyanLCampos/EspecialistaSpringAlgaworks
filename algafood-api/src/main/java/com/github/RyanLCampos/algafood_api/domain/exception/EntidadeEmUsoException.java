bd3264a015088230a17fdd73b6fb16f58c831816
RyanLCampos <ryanlcampos19@gmail.com>
2025-04-04 11:05:36 -0300

    :sparkles: Feat: implementa exception personalizada EntidadeEmUsoException

---
+++ algafood-api/src/main/java/com/github/RyanLCampos/algafood_api/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,9 @@
+package com.github.RyanLCampos.algafood_api.domain.exception;
+
+public class EntidadeEmUsoException extends RuntimeException{
+
+    public EntidadeEmUsoException(String mensagem){
+        super(mensagem);
+    }
+
+}
