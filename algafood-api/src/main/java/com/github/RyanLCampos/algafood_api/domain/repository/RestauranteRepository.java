13a87fcf81dc3c7eee5c2d2bbf85fd5d93e5ae6a
RyanLCampos <ryanlcampos19@gmail.com>
2025-04-04 12:17:15 -0300

    :sparkles: Feat: implementa um repository SDJ customizado para Restaurante

--- algafood-api/src/main/java/com/github/RyanLCampos/algafood_api/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/github/RyanLCampos/algafood_api/domain/repository/RestauranteRepository.java
@@ -2,6 +2,12 @@ package com.github.RyanLCampos.algafood_api.domain.repository;
 
 import com.github.RyanLCampos.algafood_api.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries{
+
 }
